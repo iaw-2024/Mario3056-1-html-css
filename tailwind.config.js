5f0a2d0c5556b1ce852036301c7b7b97738253df
Mario Córdoba <cord0388@gmail.com>
2024-03-25 16:21:18 -0300

    general updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   /** content: ["./listado/tailwindcss/index.html"],*/
-  content: ["./formulario/tailwindcss/index.html"],
+  content: ["./formulario/tailwindcss/css/index.html"],
   theme: {
     extend: {},
   },
